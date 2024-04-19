0e680a1ca9e09ded668a311a8c32ee491860967f
Kush Vasaniya <vasaniyakush@gmail.com>
2024-04-20 00:20:45 +0530

    added cors

--- src/app.py
+++ src/app.py
@@ -5,9 +5,22 @@ import string
 import random
 from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.metrics.pairwise import cosine_similarity
-
+from fastapi.middleware.cors import CORSMiddleware
 app = FastAPI()
+origins = [
+    "http://localhost.tiangolo.com",
+    "https://localhost.tiangolo.com",
+    "http://localhost",
+    "http://localhost:8080",
+]
 
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 class RequestBody(BaseModel):
     user_input: str
 
